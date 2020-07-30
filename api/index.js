b25d805c4e05e3b54dd3218343713677af3ca3a6
axelludwig <axel.ludwig@epitech.eu>
2020-07-30 18:52:32 +0200

    api sql connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 const port = 8080;
 
-app.get('/', function(req, res) {
+
+
+
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({ host: process.env.DB_HOST, user: process.env.DB_USER, connectionLimit: 5 });
+pool.getConnection()
+    .then(conn => {
+
+        conn.query("SELECT 1 as val")
+            .then(rows => { // rows: [ {val: 1}, meta: ... ]
+                return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+            })
+            .then(res => { // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
+                conn.release(); // release to pool
+            })
+            .catch(err => {
+                conn.release(); // release to pool
+            })
+
+    }).catch(err => {
+        //not connected
+    });
+
+
+
+app.get('/', function (req, res) {
     res.send('hello world')
 });
 
